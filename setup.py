91e195ad85f8715b3c90e608942e26c1a1a7abbd
matheus-asilva <ms.asilvas1@gmail.com>
2019-06-05 20:56:49 -0300

    First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A repo to store my walkthrough through the CodeNation's program, AceleraDev Data Science',
+    author='Matheus de Almeida Silva',
+    license='',
+)
